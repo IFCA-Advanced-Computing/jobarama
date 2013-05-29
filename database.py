f095bf0a80c2fa4a3eefa3642d4fe4822bfcbf66
Luis Cabellos <cabellos@ifca.unican.es>
2013-05-29 18:39:01 +0200

    fix database used

--- database.py
+++ database.py
@@ -5,20 +5,21 @@ import os
 import shutil
 
 #-------------------------------------------------------------------------------
+template = 'template.db'
 database = 'database.db'
 
 #-------------------------------------------------------------------------------
 def init():
     if not os.path.isfile( database ):
-        shutil.copy( 'template.db', database )
+        shutil.copy( template, database )
 
 #-------------------------------------------------------------------------------
 def checkUser( name, passwd ):
-    conn = sqlite3.connect('template.db')
+    conn = sqlite3.connect( database )
     c = conn.cursor()
-    t = (name,)
     c.execute( 'SELECT passwd FROM user WHERE name=?', (name,) )
     val = c.fetchone()
+    conn.close()
     if val is not None:
         return bcrypt.hashpw( passwd, val[0] ) == val[0]
 
