4538eca0bfe5b3fb762a15f6a5f3f14c0e1288ae
Luis Cabellos <cabellos@ifca.unican.es>
2013-05-30 17:47:18 +0200

    refactor js

--- static/js/main.js
+++ static/js/main.js
@@ -3,38 +3,38 @@
     "use strict"; // jshint ;_;
 
     var filesel = $('input[id=filesel]');
-    filesel.change(function() {
-        $('#filename').val($(this).val());
+    filesel.change( function(){
+        $('#filename').val( $(this).val() );
     });
     filesel.css( 'position', 'absolute' );
     filesel.css( 'visibility', 'hidden' );
 
     $('#filealert').css( 'visibility', 'hidden' );
 
-    $('#filebrowse').click(function() {
+    $('#filebrowse').click( function(){
         $('#filesel').click();
     });
 
-    $('#filesend').click(function() {
-        $('#filebar > .bar').replaceWith('<div id="filebar" class="bar" style="width: 0%;"></div>');
+    $('#filesend').click( function(){
+        $('#filebar > .bar').replaceWith( '<div class="bar" style="width: 0%;"></div>' );
+        $('#filebar').addClass( 'progress-striped' );
+        $('#filebar').addClass( 'active' );
         $('#filealert').css( 'visibility', 'hidden' );
-        $('#filebar').addClass('progress-striped');
-        $('#filebar').addClass('active');
         var xhr = new XMLHttpRequest();
-        if ( xhr.upload ) {
-            xhr.upload.onprogress = function(e) {
+        if( xhr.upload ){
+            xhr.upload.onprogress = function( e ){
                 var done = e.position || e.loaded, total = e.totalSize || e.total;
                 var perc = (Math.floor(done/total*1000)/10);
-                $('#filebar > .bar').css('width', '' + perc + '%' );
+                $('#filebar > .bar').css( 'width', perc + '%' );
             };
         }
-        xhr.onreadystatechange = function(e) {
-            if ( 4 == this.readyState ) {
+        xhr.onreadystatechange = function( e ){
+            if( 4 == this.readyState ){
+                $('#filebar > .bar').css( 'width', '100%' );
+                $('#filebar').removeClass( 'progress-striped' );
+                $('#filebar').removeClass( 'active' )
+                $('#fileform').css( 'visibility', 'visible' );
                 $('#filealert').css( 'visibility', 'visible' );
-                $('#filebar > .bar').css('width', '100%' );
-                $('#fileform').css( 'visibility', 'visible');
-                $('#filebar').removeClass('progress-striped');
-                $('#filebar').removeClass('active')
             }
         };
 
@@ -44,7 +44,7 @@
         var fd = new FormData( form );
         xhr.send( fd );
 
-        $('#fileform').css( 'visibility', 'hidden');
+        $('#fileform').css( 'visibility', 'hidden' );
     });
 
 }(window.jQuery);
