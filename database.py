4a3afbcdbd2efd6c2de5aeb6be881d49a4a69b82
Luis Cabellos <cabellos@ifca.unican.es>
2013-05-30 17:44:26 +0200

    add method for create empty database

--- database.py
+++ database.py
@@ -10,6 +10,26 @@ template = 'template.db'
 database = 'database.db'
 
 #-------------------------------------------------------------------------------
+def mkEmptyDatabase( dbname ):
+    if os.path.isfile( dbname ):
+        os.remove( dbname )
+
+    conn = sqlite3.connect( dbname )
+    c = conn.cursor()
+    c.execute( "CREATE TABLE user (uid INTEGER PRIMARY KEY AUTOINCREMENT, name text, passwd text)" )
+
+    name = 'admin' # same name and passwd
+    h = bcrypt.hashpw( name, bcrypt.gensalt(BCRYPT_ROUNDS) )
+    c.execute( 'INSERT INTO user VALUES (null,?,?)', (name,h) )
+    conn.commit()
+
+    c.execute( "CREATE TABLE file (fid INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER, global INTEGER, filename text)" )
+
+    conn.commit()
+
+    conn.close()
+
+#-------------------------------------------------------------------------------
 def init():
     if not os.path.isfile( database ):
         shutil.copy( template, database )
