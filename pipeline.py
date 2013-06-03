94028f6ca0c0cf61a078459c2fe2a31bab925bb4
Luis Cabellos <cabellos@ifca.unican.es>
2013-06-03 10:26:40 +0200

    change to synchronous job running

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,23 @@
 #-------------------------------------------------------------------------------
 import random
 import database
+import multiprocessing
 
 #-------------------------------------------------------------------------------
 def startJob( user, var1, fileid ):
     jobid = database.createJob( user )
 
+    p = multiprocessing.Process( target=runjob, args=(jobid, var1, fileid ) )
+    p.start()
+
+#-------------------------------------------------------------------------------
+def runjob( jobid, var1, fileid ):
     print "RUNNING JOB " + str(jobid)
     print "  var1 = " + var1
     print "  fileid = " + str(fileid)
+
+    # stagein
+
+    # submit
 
 #-------------------------------------------------------------------------------
